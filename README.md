f9212a50d69194a96d9403369f3ed3b243dd85b8
arumpf <adam.rumpf@gmail.com>
2020-10-08 11:15:18 -0400

    Adding some more methods and descriptions to the documentation.

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ A Python class for representing and performing calculations with angles.
 This is a small class meant to simplify common operations with angle measures. The convention used for the arithmetic and comparison operations is meant to capture the idea that we are primarily interested in the smallest angle between two measures, regardless of the numbers, themselves. In particular this includes the following conventions:
 
 * Angles that differ by an integer number of revolutions are considered equivalent.
-* Angle differences 
+* Output angle values are limited in size to one full revolution (e.g. all radian Angle objects maintain a measure between 0 and 2π).
+* Angle differences and comparisons are based on the smallest angle between the two input angles, and on whether the second angle is closer to being clockwise or counterclockwise from the first (positive differences indicate counterclockwise movement, while negative differences indicate clockwise).
 
 Both radian and degree measure (or any arbitrary subdivision of the circle) are supported. Methods perform calculations and return results using the measure of their own angle object, converting other angles when necessary.
 
@@ -19,14 +20,27 @@ The following is a brief description of selected attributes, custom methods, and
 
 ### Attributes
 
+#### Static Attributes
+
+* `rad_str (set)` -- Set of accepted `str` names for radian measure.
+* `deg_str (set)` -- Set of accepted `str` names for degree measure.
+* `grad_str (set)` -- Set of accepted `str` names for gradian measure.
+
+#### Class Attributes
+
 * `measure (float)` -- Current measure of the angle.
-* `mod (float)` -- Measure of a complete revolution (e.g. 2pi for radian measure, 360 for degree measure).
+* `mod (float)` -- Measure of a complete revolution (e.g. 2π for radian measure, 360 for degree measure).
 * `unit (str)` -- Name of the  unit of measure.
 
-### Custom Methods
+### Methods
 
-*
+* `__init__([measure[, mod]])` -- `Angle` class constructor. Accepts the following keyword arguments:
+  * `measure (float) [0.0]` -- Initial angle measure.
+  * `mod (int, float, or str) ["radians"]` -- Specifies measure unit. A numerical argument is treated as the measure of a full revolution, while a string argument is taken as the name of a standard unit (radians, degrees, or gradians).
+* `convert(mod)` -- Returns the angle's measure converted to a different unit.
 
-### Overloaded Methods
+### Operators
 
-*
+In all of the following descriptions, `a` represents the `Angle` object in question.
+
+* `a + b` -- Returns an `Angle` object whose unit matches that of `a` and whose measure is the sum of the measures of `a` and `b`. If `b` is a number, then this is treated as a measure (with the same unit as `a`). If `b` is another `Angle` object, then its measure is first converted to the unit of `a` before being added.
--- angles.py
+++ angles.py
@@ -4,10 +4,15 @@ class Angle:
     """A Python class for representing and performing calculations with angles.
     """
 
+    # Static attributes for accepted unit names
+    rad_str = {"radians", "radian", "rad", "r"}
+    deg_str = {"degrees", "degree", "deg", "d"}
+    grad_str = {"gradians", "gradian", "grad", "g"}
+
     #-------------------------------------------------------------------------
 
     def __init__(self, measure=0.0, mod="radians"):
-        """Angle.__init__([mod]) -> Angle
+        """Angle.__init__([measure[, mod]]) -> Angle
         Angle constructor.
 
         Keyword arguments:
@@ -32,13 +37,13 @@ class Angle:
         # Attempt to parse string mod argument
         if type(mod) == str:
             # Search through recognized words
-            if mod in {"radians", "radian", "rad", "r"}:
+            if mod in Angle.rad_str:
                 self.mod = 2*math.pi
                 self.unit = "rad"
-            elif mod in {"degrees", "degree", "deg", "d"}:
+            elif mod in Angle.deg_str:
                 self.mod = 360.0
                 self.unit = "deg"
-            elif mod in {"gradians", "gradian", "grad", "g"}:
+            elif mod in Angle.grad_str:
                 self.mod = 400.0
                 self.unit = "grad"
             else:
@@ -57,6 +62,9 @@ class Angle:
         elif mod == 400.0:
             self.unit = "grad"
 
+        # Bound measure
+        self.measure = self.measure % self.mod
+
     #-------------------------------------------------------------------------
 
     def __str__(self):
@@ -69,7 +77,118 @@ class Angle:
 
         return str(self.measure) + " " + self.unit
 
+    #-------------------------------------------------------------------------
+
+    def convert(self, new_mod):
+        """Angle.convert(mod) -> float
+        Converts the angle measure to a different unit.
+
+        Positional arguments:
+        mod (str or float) ["radians"] -- angle unit, or measure of one full
+            revolution
+        """
+
+        # Attempt to parse string mod argument
+        if type(new_mod) == str:
+            # Search through recognized words
+            if new_mod in Angle.rad_str:
+                new_mod = 2*math.pi
+            elif new_mod in Angle.deg_str:
+                new_mod = 360.0
+            elif new_mod in Angle.grad_str:
+                new_mod = 400.0
+            else:
+                # If unrecognized, raise a value error
+                raise ValueError("unrecognized unit name string")
+        else:
+            # Otherwise attempt to parse numerical mod argument
+            new_mod = float(new_mod)
+
+        # Convert measure as a fraction of a complete revolution
+        return (self.measure/self.mod)*new_mod
+
+    #-------------------------------------------------------------------------
+
+    def __abs__(self):
+        """Angle.__abs__() -> float
+        Returns the absolute value of the angle's measure's modulus.
+
+        The output of this method is always between 0.0 and the full
+        revolution measure.
+        """
+
+        return self.measure % self.mod
+
+    #-------------------------------------------------------------------------
+
+    def __add__(self, b):
+        """Angle.__add__(b) -> Angle
+        Angle addition operator.
+
+        Returns an Angle object whose measure represents the sum of two angle
+        measures. The unit of the returned object matches that of the current
+        object.
+
+        Positional arguments:
+        b (Angle, int, or float) -- angle to add to the current measure
+
+        Adding a number to the current Angle treats the number as a measure of
+        the same unit as the current object.
+
+        Adding an Angle to the current Angle results in the other measure
+        being converted to the current object's unit, after which the two are
+        added.
+        """
+
+        # If given an Angle object, convert its measure to the current unit
+        if type(b) == Angle:
+            b = b.convert(self.mod)
+
+        # Sum the measures, restricted to one revolution
+        m = (self.measure + b) % self.mod
+
+        # Return an Angle object
+        return Angle(measure=m, mod=self.mod)
+
+    #-------------------------------------------------------------------------
+
+    def __sub__(self, b):
+        """Angle.__sub__(b) -> Angle
+        Angle subtraction operator.
+
+        Returns an Angle object whose measure represents the difference
+        between two angle measures. The unit of the returned object matches
+        that of the current object.
+
+        The returned measure is the smallest acute angle between the current
+        object's measure and the given measure (after conversion), positive if
+        this acute angle positions the current angle counterclockwise relative
+        to the other and negative otherwise.
+
+        Positional arguments:
+        b (Angle, int, or float) -- angle to subtract from the current measure
+
+        Subtracting a number from the current Angle treats the number as a
+        measure of the same unit as the current object.
+
+        Subtracting an Angle from the current Angle results in the other
+        measure being converted to the current object's unit, after which the
+        two are subtracted,
+        """
+
+        # If given an Angle object, convert its measure to the current unit
+        if type(b) == Angle:
+            b = b.convert(self.mod)
+
+        # Find the smallest CW and CCW differences between the angles
+        ###
+        pass
+
 #=============================================================================
 
-temp = Angle(10, 2*math.pi)
-print(temp)
+a = Angle(1, 2*math.pi)
+b = Angle(180, "deg")
+c = Angle(math.pi, 2*math.pi)
+print(a - b)
+print(b - a)
+print(c - a)
