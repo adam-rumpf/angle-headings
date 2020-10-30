fad4c989ef0a910c5015b673dc1567cb68f9ceb4
arumpf <adam.rumpf@gmail.com>
2020-10-30 17:16:27 -0400

    Preparing to redo code to normalize angles between -1/2 and 1/2 revolution, and using the @property decorator.

--- angles.py
+++ angles.py
@@ -2,6 +2,10 @@
 ### We might be able to make this work more naturally with negation corresponding to subtraction.
 
 ### Methods to overwrite:
+### __add__            a + b
+### __sub__            a - b
+### __neg__            -a
+### __abs__            |a|
 ### __floordiv__       a // b
 ### __int__            integer version of self
 ### __float__          float version of self
@@ -101,23 +105,6 @@ class Angle:
         elif mod == 400.0:
             self.unit = "grad"
 
-        # Normalize measure
-        self._normalize()
-
-    #-------------------------------------------------------------------------
-
-    def _normalize(self):
-        """Angle._normalize() -> None
-        Normalizes the measure to be between 0 and 1 full revolutions.
-
-        This private method should be called at the beginning of all other
-        methods to ensure that a standardized angle measure is used, since all
-        calculations assume an angle measure between 0 (inclusive) and 1 (non-
-        inclusive) full revolutions.
-        """
-
-        self.measure %= self.mod
-
     #-------------------------------------------------------------------------
 
     def __str__(self):
@@ -127,8 +114,6 @@ class Angle:
         Returns the measure of the angle as a string, along with an
         abbreviation of the angle unit.
         """
-
-        self._normalize()
 
         return str(self.measure) + " " + self.unit
 
@@ -145,8 +130,6 @@ class Angle:
         The resulting measure is between 0 and 1 full revolutions (relative to
         the given unit).
         """
-
-        self._normalize()
 
         # Attempt to parse string mod argument
         if type(new_mod) == str:
@@ -166,122 +149,6 @@ class Angle:
 
         # Convert measure as a fraction of a complete revolution
         return ((self.measure/self.mod) % 1.0)*new_mod
-
-    #-------------------------------------------------------------------------
-
-    def __abs__(self):
-        """Angle.__abs__() -> float
-        Angle absolute value operator: abs(a)
-
-        Returns the absolute difference between the current Angle object's
-        measure and 0, as a float.
-
-        Specifically, the returned value is the magnitude of the smallest
-        rotation that would move this Angle to 0. If this object's measure is
-        between 0 and one half revolution, then its absolute value will simply
-        equal its measure.  If its measure is between one half and one full
-        revolution, then the supplement of its measure will be returned.
-
-        For example, the absolute values of a 10 degree Angle object and a 350
-        degree Angle object would both be 10, since in both cases 10 degrees
-        (either clockwise or counterclockwise) is the smallest change needed
-        to move the angle to 0.
-        """
-
-        self._normalize()
-
-        return min(self.measure % self.mod,
-                   (self.mod-self.measure) % self.mod)
-
-    #-------------------------------------------------------------------------
-
-    def __add__(self, b):
-        """Angle.__add__(b) -> Angle
-        Angle addition operator: a + b
-
-        Returns an Angle object whose measure represents the sum of two angle
-        measures. The unit of the returned object matches that of the current
-        object.
-
-        Positional arguments:
-        b (Angle, int, or float) -- angle to add to the current measure
-
-        Adding a number to the current Angle treats the number as a measure of
-        the same unit as the current object.
-
-        Adding an Angle to the current Angle results in the other measure
-        being converted to the current object's unit, after which the two are
-        added.
-        """
-
-        self._normalize()
-
-        # If given an Angle object, convert its measure to the current unit
-        if type(b) == Angle:
-            b = b.convert(self.mod)
-
-        # Sum the measures, restricted to one revolution
-        m = (self.measure + b) % self.mod
-
-        # Return an Angle object
-        return Angle(m, self.mod)
-
-    #-------------------------------------------------------------------------
-
-    def __neg__(self):
-        """Angle.__neg__() -> Angle
-        Angle negation operator: -a
-
-        Returns an Angle object whose measure is the conjugate of this
-        object's measure (i.e. a nonnegative angle that can be added to this
-        object's measure to yield one full revolution).
-        """
-
-        self._normalize()
-
-        return Angle((self.mod - self.measure) % self.mod, self.mod)
-
-    #-------------------------------------------------------------------------
-
-    def __sub__(self, b):
-        """Angle.__sub__(b) -> Angle
-        Angle subtraction operator: a - b
-
-        Returns an Angle object whose measure represents the difference
-        between two angle measures. The unit of the returned object matches
-        that of the current object.
-
-        The returned measure is the smallest acute angle between the current
-        object's measure and the given measure (after conversion), positive if
-        this acute angle positions the current angle counterclockwise relative
-        to the other and negative otherwise.
-
-        For example, subtracting a 10 degree Angle from a 350 degree Angle
-        yields a +20 degree Angle object, since 20 degrees CCW is the smallest
-        angle from 350 to 10. On the other hand, subtracting 350 degrees from
-        10 degrees yields -20 degrees, since 20 degrees CW is the smallest
-        angle from 10 to 350.
-
-        Positional arguments:
-        b (Angle, int, or float) -- angle to subtract from the current measure
-
-        Subtracting a number from the current Angle treats the number as a
-        measure of the same unit as the current object.
-
-        Subtracting an Angle from the current Angle results in the other
-        measure being converted to the current object's unit, after which the
-        two are subtracted.
-        """
-
-        self._normalize()
-
-        # If given an Angle object, convert its measure to the current unit
-        if type(b) == Angle:
-            b = b.convert(self.mod)
-
-        # Find the smallest CW and CCW differences between the angles
-        ###
-        pass
 
 #=============================================================================
 
